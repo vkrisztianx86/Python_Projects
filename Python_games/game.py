b15be68b092e2ce0b36a8c94be7c873659c4b9d7
Krisztian Vereczki <vkrisztianx86@outlook.com>
2023-08-20 15:55:55 +0200

    game

--- Python_games/game.py
+++ Python_games/game.py
@@ -10,14 +10,37 @@ window.title('PONG')
 window.tracer(0)
 
 #Left hand side bat
-left_hand_side_bat = turtle.Turtle()
-left_hand_side_bat.speed(0)
-left_hand_side_bat.shape('square')
-left_hand_side_bat.shapesize(stretch_wid=5, stretch_len=1)
-left_hand_side_bat.color('white')
-left_hand_side_bat.penup()
-left_hand_side_bat.goto(-350,0)
-#time.sleep(15)
+left_bat = turtle.Turtle()
+left_bat.speed(0)
+left_bat.shape('square')
+left_bat.shapesize(stretch_wid=5, stretch_len=1)
+left_bat.color('white')
+left_bat.penup()
+left_bat.goto(-350,0)
+
+
+#Right hand side bat
+right_bat = turtle.Turtle()
+right_bat.speed(0)
+right_bat.shape('square')
+right_bat.shapesize(stretch_wid=5, stretch_len=1)
+right_bat.color('white')
+right_bat.penup()
+right_bat.goto(350,0)
+
+#Ball
+ball = turtle.Turtle()
+ball.speed()
+ball.shape('circle')
+ball.color('white')
+ball.penup()
+ball.goto(0,0)
+
+def right_bat_up():
+   y = left_bat.ycor()
+   y += 30
+   left_bat.sety(y)
+
 while True:
    window.update()
 
