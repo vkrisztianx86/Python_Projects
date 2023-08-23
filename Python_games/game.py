33887297a44bb7afd793dc26cb9fe3dedf960309
Krisztian Vereczki <vkrisztianx86@outlook.com>
2023-08-23 14:13:14 +0200

    game

--- Python_games/game.py
+++ Python_games/game.py
@@ -4,37 +4,51 @@ import turtle
 # WINDOW
 window = turtle.Screen()
 window.setup(width=800, height=600)
-window.bgcolor('brown')
+window.bgcolor('grey')
 window.title('PONG')
 window.tracer(2)
 
-# Left hand side bat
+# LEFT HAND SIDE BAT
 left_bat = turtle.Turtle()
 left_bat.speed(0)
 left_bat.shape('square')
 left_bat.shapesize(stretch_wid=5, stretch_len=1)
-left_bat.color('white')
+left_bat.color('black')
 left_bat.penup()
 left_bat.goto(-350, 0)
 
-# Right hand side bat
+# RIGHT HAND SIDE BAT
 right_bat = turtle.Turtle()
 right_bat.speed(0)
 right_bat.shape('square')
 right_bat.shapesize(stretch_wid=5, stretch_len=1)
-right_bat.color('white')
+right_bat.color('brown')
 right_bat.penup()
 right_bat.goto(350, 0)
 
-# Ball
+# BALL
 ball = turtle.Turtle()
 ball.speed(0)
 ball.shape('circle')
-ball.color('white')
+ball.color('blue')
 ball.penup()
 ball.goto(0, 0)
 ball.changesX = 1
 ball.changesY = -1
+
+# SCORES
+right_score = 0
+left_score = 0
+
+score = turtle.Turtle()
+score.speed(0)
+score.color('blue')
+score.penup()
+score.hideturtle()
+score.goto(0, 260)
+
+score.write(f'Left-side player: {left_score} Right-side player: {right_score} ',
+            align='center', font=('Curier', 24, 'normal'))
 
 def left_bat_up():
     y = left_bat.ycor()
@@ -72,34 +86,42 @@ window.onkey(right_bat_down, 'k')
 
 window.listen()
 
-
 while True:
-    #screen refresh
+    # screen refresh
     time.sleep(1 / 240)
     window.update()
     ball.setx(ball.xcor() + ball.changesX)
     ball.sety(ball.ycor() + ball.changesY)
 
-    #bounce from top
+    # bounce from top
     if ball.ycor() > 288:
         ball.sety(288)
         ball.changesY *= -1
 
-    #bounce from bottom
+    # bounce from bottom
     if ball.ycor() < -288:
         ball.sety(-288)
         ball.changesY *= -1
 
-    #touch right side
+    # touch right side
     if ball.xcor() > 388:
         ball.goto(0,0)
         ball.changesX *= -1
+        left_score += 1
+        score.clear()
+        score.write(f'Left-side player: {left_score} Right-side player: {right_score} ',
+                    align='center', font=('Curier', 24, 'normal'))
 
+    # touch left side
     if ball.xcor() < -388:
         ball.goto(0,0)
         ball.changesX *= -1
+        right_score += 1
+        score.clear()
+        score.write(f'Left-side player: {left_score} Right-side player: {right_score}',
+                    align='center', font=('Curier', 24, 'normal'))
 
-    #bouncing from right bat
+    # bouncing from right bat
     if right_bat.xcor() -20 < ball.xcor() < right_bat.xcor() and right_bat.ycor() - 40 < ball.ycor() < right_bat.ycor() + 40:
         ball.setx(right_bat.xcor() - 20)
         ball.changesX *= -1
